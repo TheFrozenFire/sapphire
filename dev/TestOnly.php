54d2e59e23a71ff2dad19979c45f516b4e121af6
Justin Martin <frozenfire@thefrozenfire.com>
2012-10-25 11:58:11 -0700

    Filled in documentation for TestOnly

--- dev/TestOnly.php
+++ dev/TestOnly.php
@@ -1,5 +1,12 @@
 <?php
 /**
+ * TestOnly.php
+ *
+ * @package framework
+ * @subpackage testing
+ */
+
+/**
  * Classes that implement TestOnly are only to be used during testing
  * @package framework
  * @subpackage testing
