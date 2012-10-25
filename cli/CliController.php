e19c5b0b82b78d71a52b16bd401a84ca6cb060c7
Justin Martin <frozenfire@thefrozenfire.com>
2012-10-25 10:58:36 -0700

    Filled in documentation for CliController

--- cli/CliController.php
+++ cli/CliController.php
@@ -1,5 +1,12 @@
 <?php
 /**
+ * CliController.php
+ *
+ * @package framework
+ * @subpackage cron
+ */
+
+/**
  * Base class invoked from CLI rather than the webserver (Cron jobs, handling email bounces).
  * You can call subclasses of CliController directly, which will trigger a
  * call to {@link process()} on every sub-subclass. For instance, calling
@@ -11,10 +18,20 @@
  */
 abstract class CliController extends Controller {
 	
+	/**
+	 * Allowed controller actions
+	 * @static
+	 * @var array
+	 */
 	static $allowed_actions = array(
 		'index'
 	);
-
+	
+	/**
+	 * init
+	 *
+	 * @return void
+	 */
 	public function init() {
 		parent::init();
 		// Unless called from the command line, all CliControllers need ADMIN privileges
@@ -22,7 +39,12 @@ abstract class CliController extends Controller {
 			return Security::permissionFailure();
 		}
 	}
-
+	
+	/**
+	 * index
+	 *
+	 * @return void
+	 */
 	public function index() {
 		foreach(ClassInfo::subclassesFor($this->class) as $subclass) {
 			echo $subclass . "\n";
@@ -33,7 +55,11 @@ abstract class CliController extends Controller {
 	}
 
 	/**
+	 * process
+	 * 
 	 * Overload this method to contain the task logic.
+	 *
+	 * @return void
 	 */
 	public function process() {}
 
