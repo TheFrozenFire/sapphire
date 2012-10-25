e27aee1b315989e951928994c25154d29c3b5318
Justin Martin <frozenfire@thefrozenfire.com>
2012-10-25 12:34:09 -0700

    Filled in documentation for CliDebugView

--- dev/CliDebugView.php
+++ dev/CliDebugView.php
@@ -1,5 +1,12 @@
 <?php
 /**
+ * CliDebugView.php
+ *
+ * @package framework
+ * @subpackage dev
+ */
+
+/**
  * A basic HTML wrapper for stylish rendering of a developement info view.
  * Used to output error messages, and test results.
  * 
@@ -8,23 +15,35 @@
  * 
  * @todo Perhaps DebugView should be an interface / ABC, implemented by HTMLDebugView and CliDebugView?
  */
-
 class CliDebugView extends DebugView {
 
 	/**
 	 * Render HTML header for development views
+	 * 
+	 * @param SS_HTTPRequest $httpRequest
+	 * @return void
 	 */
 	public function writeHeader($httpRequest = null) {
 	}
 	
 	/**
 	 * Render HTML footer for development views
+	 * 
+	 * @return void
 	 */
 	public function writeFooter() {
 	}	
 
 	/**
 	 * Write information about the error to the screen
+	 * 
+	 * @param SS_HTTPRequest $httpRequest
+	 * @param int $errno
+	 * @param string $errstr
+	 * @param string $errfile
+	 * @param string $errline
+	 * @param unknown $errcontext
+	 * @return void
 	 */
 	public function writeError($httpRequest, $errno, $errstr, $errfile, $errline, $errcontext) {
 		$errorType = self::$error_types[$errno];
@@ -33,8 +52,10 @@ class CliDebugView extends DebugView {
 	}
 
 	/**
-	 * Write a fragment of the a source file
-	 * @param $lines An array of file lines; the keys should be the original line numbers
+	 * Write a fragment of a source file
+	 * 
+	 * @param array $lines An array of file lines; the keys should be the original line numbers
+	 * @param string $errline
 	 */
 	public function writeSourceFragment($lines, $errline) {
 		echo "Source\n======\n";
@@ -48,6 +69,9 @@ class CliDebugView extends DebugView {
 	
 	/**
 	 * Write a backtrace
+	 * 
+	 * @param string $trace
+	 * @return void
 	 */
 	public function writeTrace($trace = null) {
 		echo "Trace\n=====\n";
@@ -58,7 +82,9 @@ class CliDebugView extends DebugView {
 	 * Render the information header for the view
 	 * 
 	 * @param string $title
-	 * @param string $title
+	 * @param string $subtitle
+	 * @param string $description
+	 * @return void
 	 */
 	public function writeInfo($title, $subtitle, $description=false) {
 		echo wordwrap(strtoupper($title),100) . "\n";
